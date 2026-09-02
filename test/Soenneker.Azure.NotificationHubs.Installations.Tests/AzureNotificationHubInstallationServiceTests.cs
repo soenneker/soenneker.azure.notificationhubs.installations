d3ade741a3915b8d2163d4a668de38a2865c8042
soenneker <jake@soenneker.com>
2026-09-02 14:22:04 -0500

    Add cancellation tokens to async tests

--- test/Soenneker.Azure.NotificationHubs.Installations.Tests/AzureNotificationHubInstallationServiceTests.cs
+++ test/Soenneker.Azure.NotificationHubs.Installations.Tests/AzureNotificationHubInstallationServiceTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using AwesomeAssertions;
 using Microsoft.Azure.NotificationHubs;
@@ -19,22 +20,24 @@ public sealed class AzureNotificationHubInstallationServiceTests : HostedUnitTes
     }
 
     [Test]
-    public async Task Rejects_blank_installation_id()
+    public async Task Rejects_blank_installation_id(CancellationToken cancellationToken)
     {
         Func<Task> act = async () => await _util.CreateOrUpdate(
             " ",
             NotificationPlatform.FcmV1,
-            "push-channel");
+            "push-channel",
+            cancellationToken: cancellationToken);
 
         await act.Should().ThrowAsync<ArgumentException>();
     }
 
     [Test]
-    public async Task Rejects_empty_patch_set()
+    public async Task Rejects_empty_patch_set(CancellationToken cancellationToken)
     {
         Func<Task> act = async () => await _util.Patch(
             "installation-id",
-            new List<PartialUpdateOperation>());
+            new List<PartialUpdateOperation>(),
+            cancellationToken);
 
         await act.Should().ThrowAsync<ArgumentException>();
     }
